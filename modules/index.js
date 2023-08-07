6e6d0ec0feef42bb975ac92c7d108a275b99212f
Artem Moiseev <rt.rk.ra@gmail.com>
2023-08-07 18:57:48 +0300

    NewCase51

--- modules/index.js
+++ modules/index.js
@@ -1,84 +1,98 @@
-export class Slider{
-    constructor(parrent, items,dots, styleName, nextBtn, prevBtn){
+export class Slider {
+    constructor(parrent, items, dots, styleName, nextBtn, prevBtn, auto) {
         this.parrent = parrent;
         this.items = items;
-        this.dots  = dots;
+        this.dots = dots;
         this.offset = 0;
-        this.styleName = styleName
-        this.step = 2;
+        this.styleName = styleName;
+        this.step = 1;
         this.nextBtn = nextBtn;
         this.prevBtn = prevBtn;
-
-        parrent.addEventListener('click', () => {
-            if(this.step === 0) {
-                items[items.length-1].classList.remove(styleName);
-                if(dots !== null) {
-                    dots[items.length-1].classList.remove('active')
-                }
-            } else {
-                items[this.step-1].classList.remove(styleName);
-                if (dots !== null) {
-                    dots[this.step-1].classList.remove('active')
-                }
-            }
-            this.offset += items[0].clientWidth;
-            if (this.offset > (items[0].clientWidth + 40)){
-                this.offset = -items[0].clientWidth;
-            }
-            parrent.style.right = this.offset + 'px';
-            items[this.step].classList.add(styleName);
-            if(dots !== null){
-                dots[this.step].classList.add('active');
-            }
-            if (this.step === items.length-1 ){
-                this.step = 0;
-            } else{
-                this.step +=1;
-            }
-        })
-        if(nextBtn !== null) {
-            nextBtn.addEventListener('click', () => {
-                if(this.step === 0) {
-                    items[items.length-1].classList.remove(styleName);
-                } else {
-                    items[this.step-1].classList.remove(styleName);
-                }
-                this.offset += items[0].clientWidth;
-                if (this.offset > (items[0].clientWidth + 40)){
-                    this.offset = -items[0].clientWidth;
-                }
-                parrent.style.right = this.offset + 'px';
-                items[this.step].classList.add(styleName);
-                if (this.step === items.length-1 ){
-                    this.step = 0;
-                } else{
-                    this.step +=1;
-                }
-            })
+        this.auto = auto;
+        this.timer = 0;
+        this.autoSlider;
+        if(nextBtn !== null){
+            this.nextSlide();
         }
         if(prevBtn !== null){
-            prevBtn.addEventListener('click', () => {
-                if(this.step === 0) {
-                    items[items.length-1].classList.remove(styleName);
+            this.prevSlide();
+        }
+    }
+    
+    autoSlider() {
+        this.timer = setTimeout(() => {
+            this.next();
+            this.autoSlider();
+        }, 2000);
+        console.log(this.step)
+    }
+
+    next() {
+        if (this.step === 0) {
+            if (this.items[this.items.length - 1].classList.contains(this.styleName)) {
+                this.items[this.items.length - 1].classList.remove(this.styleName);
+            }
+            if (this.dots !== null) {
+                this.dots[this.items.length - 1].classList.remove('active')
+            }
+        } else {
+            if (this.items[this.step - 1].classList.contains(this.styleName)) {
+                this.items[this.step - 1].classList.remove(this.styleName);
+            }
+            if (this.dots !== null) {
+                this.dots[this.step - 1].classList.remove('active')
+            }
+        }
+        this.parrent.style.right = this.offset + 'px';
+        this.items[this.step].classList.add(this.styleName);
+        if (this.dots !== null) {
+            this.dots[this.step].classList.add('active');
+        }
+        if (this.step === this.items.length - 1) {
+            this.step = 0;
+        } else {
+            this.step += 1;
+        }
+        this.offset += this.items[0].clientWidth;
+        if (this.offset > (this.items[0].clientWidth + 40)) {
+            this.offset = -this.items[0].clientWidth;
+        }
+    };
+
+        nextSlide() {
+            this.nextBtn.addEventListener('click', () => {
+                this.prevBtn.removeAttribute('disabled');
+                if (this.step === this.items.length - 1) {
+                    this.nextBtn.disabled = true;
                 } else {
-                    items[this.step-1].classList.remove(styleName);
+                    this.nextBtn.disabled = false;
+                    this.step++;
+                    this.offset += this.items[0].clientWidth;
+                    if (this.offset > (this.items[0].clientWidth + 40)) {
+                        this.offset = -this.items[0].clientWidth;
+                    }
+                    this.parrent.style.right = this.offset + 'px';
                 }
-                this.offset -= items[0].clientWidth;
-                if (this.offset < -items[0].clientWidth){
-                    this.offset = items[0].clientWidth;
-                }
-                parrent.style.right = this.offset + 'px';
-                items[this.step].classList.add(styleName);
-                if (this.step === items.length-1 ){
-                    this.step = 0;
-                } else{
-                    this.step +=1;
-                }
+    
             })
 
         }
-        
-    }
+
+        prevSlide() {
+            this.prevBtn.addEventListener('click', () => {
+                this.nextBtn.removeAttribute('disabled');
+                if (this.step === 0) {
+                    this.prevBtn.setAttribute('disabled', true)
+                } else {
+                    this.step -= 1;
+                    this.offset -= this.items[0].clientWidth;
+                    if (this.offset < -this.items[0].clientWidth) {
+                        this.offset = this.items[0].clientWidth;
+                    }
+                    this.parrent.style.right = this.offset + 'px';
+                }
+            })
+        }
 
 }
 
--- scripts/script.js
+++ scripts/script.js
@@ -3,13 +3,14 @@ import Slider from '../modules/index.js';
 
 const impressiveSlider = document.querySelector('.impressive__sliderline');
 const impressiveItems = document.querySelectorAll('.impressive__item');
-const nextBtn = document.querySelector('.impressive__next');
-const prevBtn = document.querySelector('.impressive__prev');
+const nextBtn = document.getElementById('impressive__next');
+const prevBtn = document.getElementById('impressive__prev');
 
 const clientSlider = document.querySelector('.client__sliderline');
 const clientItems = document.querySelectorAll('.client__item');
 const clientDots = document.querySelectorAll('.client__active');
 
 
-const slider1 = new Slider(impressiveSlider, impressiveItems, null, "none", nextBtn, prevBtn);
-const slider2 = new Slider(clientSlider, clientItems, clientDots, "client__focus", null, null);
+const slider1 = new Slider(impressiveSlider, impressiveItems, null, "none", nextBtn, prevBtn, false);
+const slider2 = new Slider(clientSlider, clientItems, clientDots, "client__focus", null, null, true);
+slider2.autoSlider()
